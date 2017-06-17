24f069859f61f4fa5ad314666013948ad514ad84
Hinesh Patel <hineshpatel0@googlemail.com>
2017-06-17 19:05:04 +0100

    Enchance search feature by adding highlighting and deeper search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def index
     if params[:term]
-      @users = User.search_by_full_name(params[:term])
+      @users = User.search_by_full_name(params[:term]).with_pg_search_highlight
     else
       @users = User.all
     end
