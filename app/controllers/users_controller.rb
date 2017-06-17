b3fc992eeda141ee863c9afb2b13eeab32f6de99
Hinesh Patel <hineshpatel0@googlemail.com>
2017-06-17 16:36:07 +0100

    Add search functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    if params[:term]
+      @users = User.search_by_full_name(params[:term])
+    else
+      @users = User.all
+    end
   end
 end
